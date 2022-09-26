865252a741bb0e22b4dc14c31e9a8f51ed944b64
zsheperdigian <zsheperdigian@gmail.com>
2022-09-25 21:05:33 -0700

    added more to the seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -31,7 +31,7 @@ CREATE TABLE employee(
     last_name VARCHAR(30),
     role_id: INT,
     manager_id INT,
-    PRimary KEY (id)
+    PRIMARY KEY (id)
     FOREIGN KEY (role_id)
     REFERENCES role(id)
 
